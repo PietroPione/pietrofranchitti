163450dfc514c4bca4fd1d98b2de6db4e52affd0
PietroPione <pietrofrlavoro@gmail.com>
2025-04-23 15:57:40 +0200

    fix: transizione iphone

--- app/components/heroHome/StarWarsTitle.jsx
+++ app/components/heroHome/StarWarsTitle.jsx
@@ -33,8 +33,22 @@ function StarWarsTitle({ titoloHero }) {
         titoloHero && (
             <motion.div
                 ref={scope}
-                style={{ fontFamily: "SwFont" }}
+                style={{ fontFamily: "SwFont", willChange: "transform, opacity" }}
                 className="text-60 md:text-75 text-starwars-yellow leading-none text-center"
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+                onAnimationComplete={() => {
+                    setTimeout(() => {
+                        animate(
+                            scope.current,
+                            { y: ["0%", "-33%"], scale: 0.75 },
+                            { duration: 1, ease: "easeOut" }
+                        ).then(() => {
+                            localStorage.setItem("firstAnimationCompleted", "true");
+                        });
+                    }, 1000);
+                }}
             >
                 <PrismicRichText field={titoloHero} />
             </motion.div>
